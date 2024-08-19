467f936827f6f7ebe7b2ecc1e065993667d3dea7
Jacob Errington <git@mail.jerrington.me>
2024-08-19 10:46:54 -0400

    organize problem by category

--- src/components/ProblemList.tsx
+++ src/components/ProblemList.tsx
@@ -8,16 +8,31 @@ export interface ProblemListProps {
 }
 
 export default function ProblemList({ problems }: ProblemListProps) {
-    return <nav>
-      <h1> Coding Cat </h1>
-      <ul>
-        { (problems as Problem[]).map(
+    const groupedProblems = problems.reduce<Record<string, Problem[]>>((acc, problem) => {
+        const category = problem.meta.category;
+        if (!acc[category]) {
+            acc[category] = [];
+        }
+        acc[category].push(problem);
+        return acc;
+    }, {});
+
+    return (
+    <nav>
+      <h1> Coding Cat Problems </h1>
+
+      {Object.keys(groupedProblems).map((category) => (
+          <div key={category}>
+          <h3>{category}</h3>
+            <ul>
+        { (groupedProblems[category] as Problem[]).map(
             (p) => <li key={p.meta.name}>
                 <Link to={`/problems/${p.meta.name}`}> {p.meta.title} </Link>
             </li>,
-        )
-        }
+        )}
       </ul>
-    </nav>;
+      </div>
+      ))}
+    </nav>);
 }
 
