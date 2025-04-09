9f6ba4e8fb6750c41000cb2805fa033390fa27b0
Kristian Garkov <kristian.garkov@dawsoncollege.qc.ca>
2025-04-09 13:16:25 -0400

    add sample test

--- src/setup.test.ts
+++ src/setup.test.ts
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// sample test for now
+it ("1 + 1", () => {
+  expect(1 + 1).toEqual(2);
+});
