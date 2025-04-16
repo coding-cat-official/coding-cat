1b5cc9139fcb920f4777d81cbdaed16ace4bd0ae
Kristian Garkov <kristian.garkov04@gmail.com>
2025-04-16 12:25:40 -0400

    improve styling of login page

--- src/routes/Auth.tsx
+++ src/routes/Auth.tsx
@@ -1,47 +1,51 @@
 import { FormEvent, useState } from 'react'
 import { supabase } from '../supabaseClient'
+import { Box, Button, FormLabel, Input, Stack, Typography } from '@mui/joy';
 
 export default function Auth() {
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
   const handleLogin = async (event: FormEvent) => {
     event.preventDefault();
+    setError("");
+    setSuccess("");
 
     setLoading(true);
     const { error } = await supabase.auth.signInWithOtp({ email });
 
     if (error) {
-      alert(error.message);
+      setError(error.message);
     } else {
-      alert('Check your email for the login link!');
+      setSuccess('Check your email for the login link!');
     }
+
     setLoading(false);
   }
 
   return (
-    <div className="row flex flex-center">
-      <div className="col-6 form-widget">
-        <h1 className="header">Coding Cat</h1>
-        <p className="description">Sign in via confirmation link with your email below</p>
-        <form className="form-widget" onSubmit={handleLogin}>
-          <div>
-            <input
-              className="inputField"
-              type="email"
-              placeholder="Your email"
-              value={email}
-              required={true}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div>
-            <button className={'button block'} disabled={loading}>
-              {loading ? <span>Loading</span> : <span>Send confirmation link</span>}
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+    <Stack sx={{ width: "100%" }} direction="column" spacing="20px" justifyContent="center" alignItems="center">
+      <Typography level="h2">Login</Typography>
+      <form style={{ width: "25%", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", gap: "20px" }} onSubmit={handleLogin}>
+        <Box sx={{ width: "100%" }}>
+          <FormLabel>Email</FormLabel>
+          <Input
+            className="inputField"
+            type="email"
+            placeholder="Enter your email..."
+            value={email}
+            required={true}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Box>
+        <Button disabled={loading} type="submit">
+          {loading ? <span>Loading</span> : <span>Send confirmation link</span>}
+        </Button>
+      </form>
+      { !!error && <Typography color="danger">{error}</Typography> }
+      { !!success && <Typography color="success">{success}</Typography> }
+    </Stack>
   )
 }
