0e2608d2d99e68e77890c1790c8b4ca63bca87a2
Kristian Garkov <kristian.garkov04@gmail.com>
2025-04-24 11:38:50 -0400

    add search bar component

---
+++ src/components/ProblemSearch.tsx
@@ -0,0 +1,35 @@
+import { Input } from "@mui/joy";
+import { MagnifyingGlass } from "@phosphor-icons/react";
+import { useState } from "react";
+
+export default function ExerciseSearch() {
+  const [query, setQuery] = useState("");
+
+  return (
+    <Input
+      sx={{
+        '&::before': {
+          border: '1.5px solid var(--Input-focusedHighlight)',
+          transform: 'scaleX(0)',
+          left: '2.5px',
+          right: '2.5px',
+          bottom: 0,
+          top: 'unset',
+          transition: 'transform .15s cubic-bezier(0.1,0.9,0.2,1)',
+          borderRadius: 0,
+          borderBottomLeftRadius: '64px 20px',
+          borderBottomRightRadius: '64px 20px',
+        },
+        '&:focus-within::before': {
+          transform: 'scaleX(1)',
+        },
+        fontWeight: "normal"
+      }}
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      placeholder="Search for exercises..."
+      endDecorator={<MagnifyingGlass size={23}/>}
+      size="lg"
+    />
+  )
+}
