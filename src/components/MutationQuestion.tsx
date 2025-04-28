da74a2341273885fa158fc833379d2a1e38d886b
EmmanuelleLin <linemmanuelle12@gmail.com>
2025-04-28 16:22:58 -0400

    Add td gen depending on the problem's number of mutations.

--- src/components/MutationQuestion.tsx
+++ src/components/MutationQuestion.tsx
@@ -1,43 +1,64 @@
-import {Box, Table} from '@mui/joy';
+import {Box, Button} from '@mui/joy';
+import { useState } from 'react';
 
 export default function MutationQuestion(){
 
+  const [numOfTableRows, setNumRows] = useState(1);
+  const [tableValues, setTableValues] = useState([]);
+  const teehee = [1, 2, 3, 4, 5];
+
+  const handleNewRowClick = () => {
+    if(numOfTableRows < 15){
+      setNumRows(numOfTableRows+1);
+
+    }
+  };
 
 
   return(
     <> 
-      <table className='mutation-table'>
-        <thead>
-          <tr>
-            <th>Input</th>
-            <th>M1</th>
-            <th>M2</th>
-            <th>M3</th>
-            <th>M4</th>
-            <th>Output</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-            <input type="text" width="20px" id="p1" name="p1"/>
-            ,
-            <input type="text" width="20px" id="p2" name="p2"/>
-            </td>
-            <td>✅</td>
-            <td>❌</td>
-            <td>✅</td>
-            <td>✅</td>
-            <td>
-              hallooo
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <InitialTable mutations={teehee}/>
+      <Button onClick={handleNewRowClick} className='add-mutation-button'>➕</Button>
 
       <Box className="mutation-results">
         You have found 1/4 mutations.
       </Box>
     </>
   )
+}
+
+function InitialTable({mutations, inputs}: any){
+  return(
+    <table className='mutation-table'>
+        <thead>
+          <tr>
+            <th>Input</th>
+            {mutations.map((mutant:any, index:any) => {
+              return(
+                <th key={mutant}>M{index+1}</th>
+              )
+            })
+            }
+            <th>Output</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+            <input type="text" id="p1" name="p1"/>
+            ,
+            <input type="text" id="p2" name="p2"/>
+            </td>
+            {mutations.map((mutant:any, index:any) => {
+              return(
+                <td>👻</td>
+              )
+            })}
+            <td>
+              hallooo
+            </td>
+          </tr>
+        </tbody>
+      </table>
+  );
 }
