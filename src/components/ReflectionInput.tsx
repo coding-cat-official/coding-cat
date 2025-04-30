e4ea6007a4bd8ad06cd52b5f97dcc18c049cabde
Kristian Garkov <kristian.garkov04@gmail.com>
2025-04-30 11:27:39 -0400

    add tooltip & status messages

--- src/components/ReflectionInput.tsx
+++ src/components/ReflectionInput.tsx
@@ -1,12 +1,37 @@
-import { Button, Stack, Textarea, Typography } from "@mui/joy";
+import { Button, IconButton, Stack, Textarea, Tooltip, Typography } from "@mui/joy";
+import { Info } from "@phosphor-icons/react";
 import { useState } from "react";
 
-export default function ReflectionInput() {
+interface ReflectionProps {
+  hide?: boolean;
+}
+
+export default function ReflectionInput({ hide }: ReflectionProps) {
   const [text, setText] = useState("");
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  if (hide) return <></>
 
   return (
     <Stack direction="column" width="100%" height="100%" gap={1}>
-      <Typography level="title-lg">Reflection Prompt</Typography>
+      <Stack direction="row" gap={1} alignItems="center">
+        <Typography level="title-lg">Reflection Prompt</Typography>
+        <Tooltip
+          sx={{ maxWidth: "15em" }} 
+          title="This reflection will be associated with the most recent successful submission. Make sure to submit a reflection before submitting code again." 
+          placement="right" 
+          arrow
+          open={showTooltip}
+          onOpen={() => setShowTooltip(true)}
+          onClose={() => setShowTooltip(false)}
+        >  
+          <IconButton onClick={() => setShowTooltip(!showTooltip)}>
+            <Info size={24} />
+          </IconButton>
+        </Tooltip>
+      </Stack>
       <Textarea
         sx={{ width: "100%", height: "100%", border: 2, borderRadius: 10 }}
         placeholder="Enter your reflection..."
@@ -29,7 +54,15 @@ export default function ReflectionInput() {
                 `${text.trim().split(" ").length } word(s)`
               }
             </Typography>
-            <Button disabled={!text.trim()} onClick={() => console.log(text)}>Submit</Button>
+            <Stack direction="row" alignItems="center" gap={1}>
+              {
+                !!success && <Typography level="body-sm" color="success">Successfully submitted reflection!</Typography>
+              }
+              {
+                !!error && <Typography level="body-sm" color="danger">Error submitting reflection.</Typography>
+              }
+              <Button disabled={!text.trim()} onClick={() => console.log(text)}>Submit</Button>
+            </Stack>
           </Stack>
         }
       />
