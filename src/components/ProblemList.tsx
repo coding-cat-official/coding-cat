3b105c5bdb893afc838554fc4f6a189dff34aa8c
EmmanuelleLin <linemmanuelle12@gmail.com>
2025-04-30 16:31:58 -0400

    Remove width restriction for ProblemList.

--- src/components/ProblemList.tsx
+++ src/components/ProblemList.tsx
@@ -90,7 +90,7 @@ export default function ProblemList({selectedTab, setSelectedTab, searchedProble
           <TabPanel value={selectedTab} sx={{overflowY: 'scroll', height:"80vh"}}>
               <List>
                 { problemsByType[selectedTab].map((p) => 
-                    <ListItemButton sx={{ width: "60%"}} key={p.meta.name} selected={p.meta.name === activeProblem}
+                    <ListItemButton className={"problems"} key={p.meta.name} selected={p.meta.name === activeProblem}
                         component={Link} to={`/problems/${p.meta.name}`} onClick={closeDrawer}>
                         <Stack width="100%" direction="row" justifyContent="space-between">
                           <Typography>{p.meta.title}</Typography>
