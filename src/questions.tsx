2185457a285af9f68630b307e3728ff5619e18d0
Kristian Garkov <kristian.garkov04@gmail.com>
2025-05-01 14:03:49 -0400

    add list of reflection questions

---
+++ src/questions.tsx
@@ -0,0 +1,20 @@
+export const reflectionQuestions = {
+  regular: [
+    "How did you end up solving the problem?",
+    "Why do you think your solution was correct/incorrect?",
+    "What did you learn from solving this problem?",
+    "Was this problem easy or hard for you? Why?",
+    "Was there anything particularly difficult about this problem? If so, how did you solve it?",
+    "Is there any part of the problem you still find confusing?",
+    "Which programming concepts did you use in your solution?",
+    "Are there any programming concepts you think you need to practice more?",
+    "How long did it take you to solve this problem? If you could go back in time, what would you do differently to solve this quicker?"
+  ],
+  // questions that rely on a certain condition to be met for them to make sense to ask
+  // ex: having 20% of test cases right, having multiple submissions for a certain question
+  conditional: [
+    "You got 20% of test cases right. Why do you think some test cases pass and the others don't?",
+    "How was this solution different from your last one?",
+    "How does this problem compare to the last one you did? Why?"
+  ]
+}
