cdaa12566c86a75ce640543df25e02046020ec20
EmmanuelleLin <linemmanuelle12@gmail.com>
2025-05-06 16:25:40 -0400

    Add more content to steps and CSS.

--- src/components/MainTutorial.tsx
+++ src/components/MainTutorial.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Card } from '@mui/joy';
+import { ArrowCircleLeft, ArrowCircleRight} from '@phosphor-icons/react';
 import { useState } from 'react';
 
 const tutorialSteps = [
@@ -6,25 +7,62 @@ const tutorialSteps = [
     content: () => (
       <>
         <h1>Welcome to CODING CAT 😸</h1>
-        <p>yap yap</p>
+        <p>You will do all the exercises Eric will require of you this semester here</p>
+        <h3>╮(╯▽╰)╭</h3>
       </>
     )
   },
   {
     content: () => (
-      <p>Open the navigation bar here</p>
+      <>
+        <h2>To navigate to exercises</h2>
+        <p>Click on the bar on your left</p>
+        <h3>👈(ﾟヮﾟ) </h3>
+      </>
     )
   },
   {
     content: () => (
-      <h2>Let's choose a problem!</h2>
-
+      <>
+        <h2>Problem Types</h2>
+        <p>Problems are categorized by type: coding, haystack, and mutation.</p>
+        <p>Image of the different tabs</p>
+      </>
+    )
+  },
+  {
+    content: () => (
+      <>
+        <h2>Coding</h2>
+        <p>These are normal coding questions with a description.</p>
+        <p>Image of a coding question</p>
+      </>
+    )
+  },
+  {
+    content: () => (
+      <>
+        <h2>Coding</h2>
+        <p>When you press "Run", your code will be run and compared to how the function is supposed to return.</p>
+        <ul>
+          <li><b>Input</b></li>
+          <li>What is provided to your code</li>
+          <li><b>Expected output</b></li>
+          <li>What the function is supposed to return</li>
+          <li><b>Output</b></li>
+          <li>What your code returns when given the input</li>
+        </ul>
+        <p>Image Results table</p>
+      </>
+    )
+  },
+  {
+    content: () => (
+      <>
+        <h2>Let's choose a problem! (๑•̀ㅂ•́)و✧</h2>
+        <p>Explore and Have fun!</p>
+      </>
     ),
-  },
-  {
-    content: () => (
-      <p>Profile</p>
-    )
   }
 ];
 
@@ -43,10 +81,15 @@ export default function MainTutorial(){
   }
 
   return(
-    <Box>
-      <Card> {content.content()} </Card>
-      <Button onClick={previousStep}>Back</Button>
-      <Button onClick={nextStep}>Continue</Button>
+    <Box sx={{ height:"90%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+      <Card className="main-tutorial"> 
+        {content.content()} 
+        {step === tutorialSteps.length-1 ? <Button>Ok</Button> : <></>}
+      </Card>
+      <Box sx={{ display: "flex", justifyContent: "center"}} className="tutorial-icons">
+        <Button onClick={previousStep}><ArrowCircleLeft size={50} /></Button>
+        <Button onClick={nextStep}><ArrowCircleRight size={50}/></Button>
+      </Box>
     </Box>
   )
 }
