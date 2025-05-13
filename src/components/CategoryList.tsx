4355bf73a48ffcae9dbcf66be7d851a274599a01
Kristian Garkov <kristian.garkov04@gmail.com>
2025-05-13 10:41:20 -0400

    add error handling

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -110,6 +110,12 @@ export default function CategoryList({
         }
     }
 
+    if (error) {
+        return (
+            <Typography>Error fetching categories: {error}</Typography>
+        )
+    }
+
     return (
         <List component="nav" sx={{ py: 2}}>
             {categories.sort((a,b) => a.localeCompare(b)).map(category => {
