b98b1c40123f470feb3aca1f17f9bb9857f33f7c
Kristian Garkov <kristian.garkov04@gmail.com>
2025-05-13 14:56:43 -0400

    implement sorting by name, completed, difficulty

--- src/components/ProblemList.tsx
+++ src/components/ProblemList.tsx
@@ -76,11 +76,7 @@ export default function ProblemList({selectedTab, setSelectedTab, searchedProble
     return p.passed_tests !== p.total_tests && !solvedProblems.includes(p.problem_title);
   }).map((p) => p.problem_title);
 
-  if (Object.keys(problemsByCategory).length === 0) {
-    return (
-      <Typography>No problems found</Typography>
-    )
-  }
+  sortProblems(problemsByCategory[selectedTab] || problemsByCategory[""], solvedProblems, order, orderBy);
 
   const handleTabChange = (_: any, newValue: any) => {
     if(newValue != null){
@@ -93,6 +89,12 @@ export default function ProblemList({selectedTab, setSelectedTab, searchedProble
 
     setOrder(isAsc ? "desc" : "asc");
     setOrderBy(sortCategory);
+  }
+
+  if (Object.keys(problemsByCategory).length === 0) {
+    return (
+      <Typography>No problems found</Typography>
+    )
   }
 
   if (error) {
@@ -124,7 +126,7 @@ export default function ProblemList({selectedTab, setSelectedTab, searchedProble
           <Stack pl={2} pt={2} pb={2} width="100%" direction="row" gap={2}>
             {
               sortCategories.map((sc) => (
-                <Button variant="plain" onClick={() => handleSort(sc)}>{sc}</Button>
+                <Button variant="plain" onClick={() => handleSort(sc.toLowerCase())}>{sc}</Button>
               ))
             }
           </Stack>
@@ -168,4 +170,64 @@ function DifficultyChip({ difficulty }: { difficulty: string }) {
       <Typography sx={{ color: "black" }} textAlign="center" width="4em" level="body-sm">{difficulty}</Typography>
     </Chip>
   )
+}
+
+function sortProblems(problemList: Problem[], solvedProblems: string[], order: Order, orderBy: string) {
+  if (orderBy === "name") {
+    problemList?.sort((a, b) => {
+      if (order === "asc") return sortByName(a, b);
+      else return -sortByName(a, b);
+    });
+  }
+
+  if (orderBy === "completed") {
+    problemList?.sort((a, b) => {
+      if (order === "asc") return sortByCompleted(a, b, solvedProblems);
+      else return -sortByCompleted(a, b, solvedProblems);
+    });
+  }
+
+  if (orderBy === "difficulty") {
+    problemList?.sort((a, b) => {
+      if (order === "asc") return sortByDifficulty(a, b);
+      else return -sortByDifficulty(a, b);
+    });
+  }
+}
+
+function sortByCompleted(a: Problem, b: Problem, solvedProblems: string[]) {
+  const aIsSolved = solvedProblems.includes(a.meta.name);
+  const bIsSolved = solvedProblems.includes(b.meta.name);
+
+  if (aIsSolved && !bIsSolved) return -1;
+  if (!aIsSolved && bIsSolved) return 1;
+
+  return 0;
+}
+
+function sortByName(a: Problem, b: Problem) {
+  const nameA = a.meta.name.toLowerCase();
+  const nameB = b.meta.name.toLowerCase();
+
+  if (nameA < nameB) return -1;
+  if (nameA > nameB) return 1;
+
+  return 0;
+}
+
+function sortByDifficulty(a: Problem, b: Problem) {
+  const difficultyA = mapDifficulty(a.meta.difficulty);
+  const difficultyB = mapDifficulty(b.meta.difficulty);
+
+  return difficultyA - difficultyB;
+}
+
+function mapDifficulty(diff: string) {
+  let num = -1;
+
+  if (diff === "easy") num = 0; 
+  if (diff === "medium") num = 1; 
+  if (diff === "hard") num = 2;
+  
+  return num;
 }
