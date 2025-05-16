8cb1d7ab144a8d2a97bb94c78ad8e0f859f31a56
Kristian Garkov <kristian.garkov04@gmail.com>
2025-05-16 10:54:51 -0400

    use public-problems if env not defined

--- src/utils/getProblemSet.ts
+++ src/utils/getProblemSet.ts
@@ -1,6 +1,6 @@
 import { Problem } from "../types";
 
 export default async function getProblemSet(): Promise<Problem[]> {
-  const problemSet = process.env.REACT_APP_PROBLEM_SET;
+  const problemSet = process.env.REACT_APP_PROBLEM_SET || "public-problems";
   return (await import(`../${problemSet}/problems`)).default;
 }
