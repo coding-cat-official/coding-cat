65eb8ad42a6543ddbf23d8089686ea1c1d7acecf
EmmanuelleLin <linemmanuelle12@gmail.com>
2025-05-21 09:41:44 -0400

    Fix changing problem type run crash.

--- src/utils/mapMutantResults.ts
+++ src/utils/mapMutantResults.ts
@@ -2,7 +2,7 @@ export function mapMutantResults (evalResponse:any): Map<number, Set<boolean>>{
   const mutantResults = new Map<number, Set<boolean>>();
 
   evalResponse?.report?.forEach((row:any) => {
-    if(row.solution.equal){
+    if(row?.solution?.equal){
       row.mutations.forEach((mutation:any, index:number) => {
         if (!mutantResults.has(index)) {
           mutantResults.set(index, new Set());
