c19a462359cc8d0753b42a7b6c44a4ad7d2d9172
EmmanuelleLin <linemmanuelle12@gmail.com>
2025-05-21 11:34:11 -0400

    Add styling to Solution Code.

--- src/components/SolutionCode.tsx
+++ src/components/SolutionCode.tsx
@@ -4,11 +4,11 @@ export default function SolutionCode({problem}:any){
 
   return(
     <>
-      <Box>
+      <Box sx={{width: "100%"}}>
         <Typography level="h3">
           Solution Code
         </Typography>
-        <Box component="pre" sx={{mt: 1, p: 1, bgcolor: '#f5f5f5', borderRadius: 1, fontSize: '0.85rem', overflowX: 'auto', }}>
+        <Box component="pre" sx={{mt: 1, p: 1, bgcolor: '#f5f5f5', borderRadius: "10px", fontSize: '0.85rem', overflowX: 'auto', border:"1px solid black"}}>
           { problem.solution }
         </Box>
       </Box>
