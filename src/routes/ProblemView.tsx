0c040206ab93c476145ff87967d3c10315ebae75
Kristian Garkov <kristian.garkov04@gmail.com>
2025-05-22 14:40:01 -0400

    use unused variables

--- src/routes/ProblemView.tsx
+++ src/routes/ProblemView.tsx
@@ -319,7 +319,7 @@ function Report({ evalResponse, questionType }: ReportProps) {
                   <Typography>{i + 1}</Typography>
                 </td>
                 <td style={{ textAlign: 'center' }}>
-                  {r.equal ? '✅' : '❌'}
+                  {r.equal ? TEST_CASE_PASSED : TEST_CASE_FAILED}
                 </td>
               </tr>
             ))}
