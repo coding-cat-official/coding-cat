85a61495dc46e7bdb67faa5074ebe62d92c996df
Luke Weaver <bluefly102@gmail.com>
2026-04-20 15:37:31 -0400

    Change category to be the display name

--- scripts/create-problem.py
+++ scripts/create-problem.py
@@ -10,34 +10,35 @@
 #   ]
 # }
 
+difficulties = [
+    "easy",
+    "medium",
+    "hard"
+]
+
+categories = [
+    { "display_name": "Fundamentals", "input_name": "fundamentals" },
+    { "display_name": "List-1: Indexing", "input_name": "list_1" },
+    { "display_name": "List-2: Iterating", "input_name": "list_2" },
+    { "display_name": "List-3: Complex Loop" , "input_name": "list_3" },
+    { "display_name": "Logic" , "input_name": "logic" },
+    { "display_name": "String-1" , "input_name": "string_1" },
+    { "display_name": "String-2" , "input_name": "string_2" },
+    { "display_name": "String-3" , "input_name": "string_3" }
+]
+
+question_types = [
+    "coding",
+    "mutation",
+    "haystack"
+]
+
 def main():
-    difficulties = [
-        "easy",
-        "medium",
-        "hard"
-    ]
-
-    categories = [
-        { "display_name": "Fundamentals", "input_name": "fundamentals" },
-        { "display_name": "List-1: Indexing", "input_name": "list_1" },
-        { "display_name": "List-2: Iterating", "input_name": "list_2" },
-        { "display_name": "List-3: Complex Loop" , "input_name": "list_3" },
-        { "display_name": "Logic" , "input_name": "logic" },
-        { "display_name": "String-1" , "input_name": "string_1" },
-        { "display_name": "String-2" , "input_name": "string_2" },
-        { "display_name": "String-3" , "input_name": "string_3" }
-    ]
-
-    question_types = [
-        "coding",
-        "mutation",
-        "haystack"
-    ]
-
     new_problem = {
         "title": "",
         "name": "",
         "difficulty": "",
+        "author": "",
         "category": "",
         "question_type": []
     }
@@ -46,12 +47,7 @@ def main():
     new_problem["title"] = get_user_input("Please enter the title of the problem. This is the display name, it can have caps and spaces:\n")
     new_problem["name"] = make_snake_case(new_problem["title"])
     new_problem["difficulty"] = get_user_input("\nPlease enter the difficulty of the problem:\n", difficulties)
-
-    category_inputs = []
-    for category in categories:
-        category_inputs.append(category["input_name"])
-    new_problem["category"] = get_user_input("\nPlease enter the category of problem:\n", category_inputs)
-
+    new_problem["category"] = get_category()
     new_problem["question_type"] = get_user_input("\nPlease input the problem's question type:\n", question_types)
     print(new_problem)
 
@@ -77,6 +73,19 @@ def is_valid_input(input_str: str, whitelist: list = []):
         if not input_str in whitelist:
             return False
     return True
+
+def get_category():
+    category_inputs = []
+
+    for category in categories:
+        category_inputs.append(category["input_name"])
+    
+    category_input_name = get_user_input("\nPlease enter the category of problem:\n", category_inputs)
+    
+    for category in categories:
+        if category["input_name"] == category_input_name:
+            return category["display_name"]
+    return "huh?"
 
 def make_snake_case(name: str):
     name = name.lower()
