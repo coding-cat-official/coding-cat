2616cbff7a031b327c9180021f551b74230d817f
Luke Weaver <bluefly102@gmail.com>
2026-04-23 15:41:03 -0400

    Use os.path.join for safer pathing, and add more spaces for readability

--- scripts/create-problem.py
+++ scripts/create-problem.py
@@ -77,7 +77,9 @@ def get_problem_props() -> dict:
     new_problem["difficulty"] = get_user_input("\nPlease enter the difficulty of the problem:\n", difficulties)
     new_problem["category"] = get_category()
     new_problem["question_type"].append(get_user_input("\nPlease input the problem's question type:\n", question_types))
+    
     print(f"\nNew Problem: {new_problem}\n")
+    
     return new_problem
 
 # Gets a string from the user after printing the input_message
@@ -87,14 +89,17 @@ def get_user_input(input_message: str, whitelist: list = [], check_file_path: bo
     while True: # emulates do while loop
         if len(whitelist) > 0: # reads out possible answers if a whitelist exists
             input_message += "Possible answers:"
+            
             for option in whitelist:
                 input_message += f"\n     - {option}"
+            
             input_message += "\n"
         
         user_input = input(input_message)
         
         if is_valid_input(user_input, whitelist, check_file_path):
             break
+    
     return user_input.strip()
 
 # Ensures input_str:
@@ -106,20 +111,26 @@ def is_valid_input(input_str: str, whitelist: list, check_file_path: bool) -> bo
     if input_str is None or input_str == "":
         print("\nSorry, input was an empty string or None\n")
         return False
+    
     stripped_str = input_str.strip()
+    
     if stripped_str == "":
         print("\nSorry, input was only whitespace\n")
         return False
+    
     if not stripped_str.replace(" ", "").isalnum():
         print("\nSorry, input should only be alphanumeric characters and spaces\n")
         return False
+    
     if len(whitelist) > 0:
         if not input_str in whitelist:
             print("\nSorry, that was not one of the options.\n")
             return False
+    
     if check_file_path and os.path.exists(make_kebab_case(input_str)):
         print("\nSorry, a problem with that name already exists.\n")
         return False
+    
     return True
 
 # gets the category from user input using the categories dict
@@ -134,6 +145,7 @@ def get_category() -> str:
     for category in categories:
         if category["input_name"] == category_input_name:
             return category["display_name"]
+    
     return "huh?"
 
 def make_snake_case(name: str) -> str:
@@ -150,12 +162,15 @@ def make_kebab_case(name: str) -> str:
 def get_path_to_problem(problem: dict) -> str:
     try:
         dir_name = make_kebab_case(problem["title"])
+        base_path = os.path.join("src", "public-problems")
+        
         # even though haystack and mutation are problem_types,
         # regardless of category, they go in their respective dirs
         if new_problem["question_type"][0] == "haystack":
-            return f"src/public-problems/haystack/{dir_name}"
+            return os.path.join(base_path, "haystack", dir_name)
+        
         if new_problem["question_type"][0] == "mutation":
-            return f"src/public-problems/mutation/{dir_name}"
+            return os.path.join(base_path, "mutation", dir_name)
         
         # get snake_case name for category
         category_dir = ""
@@ -163,7 +178,9 @@ def get_path_to_problem(problem: dict) -> str:
             if category["display_name"] == new_problem["category"]:
                 category_dir = category["dir_name"]
                 break
-        return f"src/public-problems/{category_dir}/{dir_name}"
+        
+        return os.path.join(base_path, category_dir, dir_name)
+    
     except Exception as e:
         print(f"Something went wrong: {e}")
         return ""
