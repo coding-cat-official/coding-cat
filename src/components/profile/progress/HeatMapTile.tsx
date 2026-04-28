7de4549a9f7a2f7a5cd61019b40bd1b3d89e082b
Luke Weaver <bluefly102@gmail.com>
2026-04-28 16:29:15 -0400

    Implement basic heatmap for all submission

--- src/components/profile/progress/HeatMapTile.tsx
+++ src/components/profile/progress/HeatMapTile.tsx
@@ -1,13 +1,31 @@
+import { Box } from "@mui/joy";
+
 export default function HeatMapTile({ date, contributions }: { date: string, contributions: number }){
+  var heatmapColor = "gray";
+  // if there are MORE contributions than 'num', Block is 'color'
+  const thresholds = [
+    { num: 0, color: "red" },
+    { num: 3, color: "yellow" },
+    { num: 5, color: "lightgreen" },
+    { num: 10, color: "green" },
+  ]
+
+  for(const threshold of thresholds){
+    if(contributions > threshold.num){
+      heatmapColor = threshold.color;
+    }
+  }
+
   return (
     <div>
-      <button id={date}
+      <Box id={date}
         data-tooltip-id="date-tooltip"
         data-tooltip-content={`${contributions} contributions on ${date}`}
         data-tooltip-place="left"
+        sx={{ backgroundColor: `${heatmapColor}`, padding: "5px" }}
       >
-            
-      </button>
+        
+      </Box>
     </div>
   )
 }
