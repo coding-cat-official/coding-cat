83cc76ef6e92a9c4c2ea606e744de547d562d1ad
AltrosMado <vinay.sharma102@yahoo.com>
2026-04-30 15:18:26 -0400

    show switch to toggle read only on contract

--- src/components/admin/ToggleContract.tsx
+++ src/components/admin/ToggleContract.tsx
@@ -1,4 +1,4 @@
-import { Modal, Box, Typography } from "@mui/joy";
+import { Modal, Box, Typography, Switch  } from "@mui/joy";
 
 interface ToggleContractProps {
   open: boolean;
@@ -6,16 +6,16 @@ interface ToggleContractProps {
 }
 
 const styles = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  backgroundColor: 'beige',
-  border: '2px solid #000',
+  backgroundColor: "beige",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
-}
+};
 
 export default function ToggleContract({ open, handleClose }: ToggleContractProps) {
   return (
@@ -26,8 +26,15 @@ export default function ToggleContract({ open, handleClose }: ToggleContractProp
       aria-describedby="modal-modal-description"
     >
       <Box sx={styles}>
-        <Typography id="modal-modal-title">
+        <Typography id="modal-modal-title" component="h2">
           Change Read/Write on Contract
+        </Typography>
+        <Typography id="modal-modal-description">
+          Below is a switch that toggles the contract to be read-only globally. This does not apply
+          for student who have contract overrides enabled.
+        </Typography>
+        <Typography component="label" endDecorator={<Switch />}>
+            Read-Only Mode
         </Typography>
       </Box>
     </Modal>
