4ff61bb1e41f6aade3c60f881c4e3a0da20b4279
Luke Weaver <bluefly102@gmail.com>
2026-05-04 10:18:17 -0400

    Remove unused import

--- src/routes/ProblemView.tsx
+++ src/routes/ProblemView.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useLoaderData, useNavigate, useOutletContext } from 'react-router-dom';
 import Markdown from 'markdown-to-jsx';
 
