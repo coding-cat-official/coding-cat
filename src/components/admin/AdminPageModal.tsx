e8235c6783898d7b86a020f56105496065828f92
AltrosMado <vinay.sharma102@yahoo.com>
2026-05-04 12:12:47 -0400

    create modal template

---
+++ src/components/admin/AdminPageModal.tsx
@@ -0,0 +1,50 @@
+import { Modal, Box, Typography } from "@mui/joy";
+
+// Interface for Modal Props
+interface AdminPageModalProps {
+  open: boolean;
+  handleClose: () => void;
+  modalTitle : string;
+  modalDesc : string;
+}
+
+// Style for modal itself
+const styles = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  backgroundColor: "beige",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+/**
+ * A component that acts a modal template for the links in the admin dashboard
+ * @param open Defines whether the module is open or not
+ * @param handleClose Function that defines how to handle closing the modal
+ * @param modalTitle String that defines the title of the modal
+ * @param modalDesc String that defines the description of the modal
+ * @returns <AdminPageModal {...props} />
+ */
+export default function AdminPageModal({ open, handleClose, modalTitle, modalDesc }: AdminPageModalProps) {
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={styles}>
+        <Typography id="modal-modal-title" component="h2">
+          {modalTitle}
+        </Typography>
+        <Typography id="modal-modal-description">
+          {modalDesc}
+        </Typography>
+      </Box>
+    </Modal>
+  );
+}
