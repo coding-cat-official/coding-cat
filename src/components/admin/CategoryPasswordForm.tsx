53a0ef42f4981d649b437c615fcc98f825de65a3
AltrosMado <vinay.sharma102@yahoo.com>
2026-05-04 16:05:59 -0400

    create component to input and save password

---
+++ src/components/admin/CategoryPasswordForm.tsx
@@ -0,0 +1,11 @@
+import { Button, Input } from "@mui/joy";
+
+export default function CategoryPasswordForm() {
+  return (
+  <>
+    <Input placeholder="Enter Category Password" />
+    <Button color="success"> Save </Button>
+  </>
+  );
+  
+}
