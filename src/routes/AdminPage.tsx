6a1dd733f76280603a595c8be32df5d80c473e20
AltrosMado <vinay.sharma102@yahoo.com>
2026-05-04 12:13:51 -0400

    refactor to dynamically show modal per click

--- src/routes/AdminPage.tsx
+++ src/routes/AdminPage.tsx
@@ -1,12 +1,39 @@
 import { Box, Typography, Link, Card } from "@mui/joy";
 import { useState } from "react";
-import ToggleContract from "../components/admin/ToggleContract";
+import AdminPageModal from "../components/admin/AdminPageModal";
 
+interface ModalMetaData {
+  title: string;
+  desc: string;
+}
+
+// CSS styles of links
+const linkStyle = { marginBottom: 2, color: "black" };
+
+// Meta data for each link
+const links: ModalMetaData[] = [
+  {
+    title: "View Student Information",
+    desc: "Enter the email/student id of the student you want to a detailed view of",
+  },
+  {
+    title: "Toggle Public/Test Questions",
+    desc: "Below is a switch that toggles what types of questions to display to the user. You can choose to display test questions or the pubic questions",
+  },
+  {
+    title: "Modify Global Contract Permissions",
+    desc: "Below is a switch that toggles the contract to be read-only globally. This does not apply for student who have contract overrides enabled.",
+  },
+];
+
+/**
+ * Page level component that renders a box of link that render modals that allows the user to make changes to the app itself
+ * @returns <AdminPage />
+ */
 export default function AdminPage() {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  
+  const [open, setOpen] = useState<ModalMetaData | null>(null);
+  const handleOpen = (data: ModalMetaData) => setOpen(data);
+  const handleClose = () => setOpen(null);
 
   return (
     <Box
@@ -30,16 +57,23 @@ export default function AdminPage() {
         <Typography level="h2" sx={{ marginBottom: 2, color: "black" }}>
           Admin Dashboard
         </Typography>
-        <Link href="/admin/users" sx={{ color: "black", fontSize: "lg" }}>
-          View Student Information
-        </Link>
-        <Link href="/admin/problems" sx={{ color: "black", fontSize: "lg" }}>
-          Toggle Public/Test Questions
-        </Link>
-        <Link component="button" sx={{ color: "black", fontSize: "lg" }} onClick={handleOpen}>
-          Modify Global Contract Permissions
-        </Link>
-        {open && <ToggleContract open={open} handleClose={handleClose} />}
+
+        {/* Dynamically render links */}
+        {links.map((elem, index) => (
+          <Link key={index} component="button" sx={linkStyle} onClick={() => handleOpen(elem)}>
+            {elem.title}
+          </Link>
+        ))}
+
+        {/* Renders different modals data depending on what link was clicked */}
+        {open && (
+          <AdminPageModal
+            open
+            handleClose={handleClose}
+            modalTitle={open.title}
+            modalDesc={open.desc}
+          />
+        )}
       </Card>
     </Box>
   );
