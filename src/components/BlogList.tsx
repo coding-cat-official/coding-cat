322ab48a3edfec5c18132a447ee3ff14a46f2ea0
Thomas Koleboshyna <thomas.koleboshyna@gmail.com>
2026-05-08 12:12:19 -0400

    Add blog posts to sidebar menu

---
+++ src/components/BlogList.tsx
@@ -0,0 +1,59 @@
+import { List, ListItemButton, Stack, Tab, TabList, TabPanel, Tabs, Typography } from "@mui/joy";
+import { Link } from "react-router-dom";
+import { BlogPost } from "../types";
+
+
+export interface BlogListProps {
+    searchedBlogs: BlogPost[];
+    selectedTab: string;
+    setSelectedTab: (peep: string) => void;
+    selectedCategory: string | null;
+    activeBlog: string | null;
+    closeDrawer: () => void;
+}
+
+export default function BlogList({
+    searchedBlogs,
+    selectedTab,
+    setSelectedTab,
+    selectedCategory,
+    activeBlog,
+    closeDrawer,
+}: BlogListProps) {
+
+
+    const handleTabChange = (_: any, newValue: any) => {
+        if (newValue != null) {
+            setSelectedTab(newValue);
+        }
+    }
+
+    return (
+        <Stack gap={1} className="stack-problemList">
+            <List component="nav">
+                <Tabs value={selectedTab} onChange={handleTabChange}>
+                    <TabList>
+                        {Object.keys(searchedBlogs).sort().filter(Boolean).map((type) => (
+                            <Tab key={type} value={type} variant="plain" color="neutral" sx={{ fontFamily: "Silkscreen" }}>
+                                {type}
+                            </Tab>
+                        ))}
+                    </TabList>
+
+                    <TabPanel className="problemList-list" value={selectedTab} sx={{ overflowY: 'auto', height: "60vh", pt: 0 }}>
+                        <List sx={{ pt: 0 }}>
+                            {searchedBlogs?.map((b) =>
+                                <ListItemButton className="problems" key={b.meta.blog_slug} selected={b.meta.blog_slug === activeBlog}
+                                    component={Link} to={`/blogs/${b.meta.blog_slug}`} onClick={closeDrawer}>
+                                    <Stack width="100%" direction="row" justifyContent="space-between">
+                                        <Typography sx={{ fontFamily: "Victor Mono" }}>{b.meta.title}</Typography>
+                                    </Stack>
+                                </ListItemButton>,
+                            )}
+                        </List>
+                    </TabPanel>
+                </Tabs>
+            </List>
+        </Stack>
+    );
+}
