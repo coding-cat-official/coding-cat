10fe4afb8879c48c1fd68884d00282e582b2072d
AltrosMado <vinay.sharma102@yahoo.com>
2026-05-13 14:01:04 -0400

    create wip list controller for test questions

---
+++ src/components/admin/ListProtectedCategories.tsx
@@ -0,0 +1,20 @@
+import { Typography } from "@mui/joy";
+import Box from "@mui/joy/Box";
+
+export function ListProtectedCategories() {
+  return (
+    <Box
+      sx={{
+        bgcolor: "#b2f2bb",
+        border: "2px solid",
+        borderColor: "#006400",
+        borderRadius: 2,
+        p: 2,
+      }}
+    >
+      <Typography> 
+        testing
+      </Typography>
+    </Box>
+  );
+}
