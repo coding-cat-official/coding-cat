d8fa4a9be8b097bf5e3d20402f4e3e0f73dcc5c0
AltrosMado <vinay.sharma102@yahoo.com>
2026-05-13 15:09:29 -0400

    update visablility and displaying current test-categories

--- src/components/admin/ListProtectedCategories.tsx
+++ src/components/admin/ListProtectedCategories.tsx
@@ -1,20 +1,39 @@
-import { Typography } from "@mui/joy";
+import { List, ListItem, Switch, Typography } from "@mui/joy";
 import Box from "@mui/joy/Box";
 
-export function ListProtectedCategories() {
-  return (
+interface ListProtectedCategoriesProps {
+  testCategories: string[];
+  visibility: boolean
+}
+
+const boxStyles = {
+  bgcolor: "#b2f2bb",
+  border: "2px solid",
+  borderColor: "#006400",
+  borderRadius: 2,
+  p: 2,
+};
+
+/**
+ * A component that display and controls what test categories to display
+ * @param testCategories List of strings representing the test categories 
+ * @returns <ListProtectedCategories {...props} />
+ */
+export function ListProtectedCategories({ testCategories, visibility }: ListProtectedCategoriesProps) {
+  return visibility ? (
     <Box
-      sx={{
-        bgcolor: "#b2f2bb",
-        border: "2px solid",
-        borderColor: "#006400",
-        borderRadius: 2,
-        p: 2,
-      }}
+      sx={boxStyles}
     >
-      <Typography> 
-        testing
+      <Typography>
+        <List>
+          {testCategories.map((elem) => (
+            <ListItem key={elem}>
+              {elem}
+              <Switch />
+            </ListItem>
+          ))}
+        </List>
       </Typography>
     </Box>
-  );
+  ) : null;
 }
