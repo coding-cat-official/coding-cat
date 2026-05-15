9a4dbf74d667e3e1d7f2717b1bcf207849f41f77
AltrosMado <vinay.sharma102@yahoo.com>
2026-05-15 14:02:05 -0400

    make sure old/removes test categories are removed from db

--- src/hooks/useTestCategoriesSync.tsx
+++ src/hooks/useTestCategoriesSync.tsx
@@ -3,6 +3,10 @@ import { Problem } from "../types";
 import { TEST_CATEGORY_PATTERN } from "../utils/constants";
 import { supabase } from "../supabaseClient";
 
+/**
+ * A hook that syncs the test categories to the db
+ * @param problems the full problem set
+ */
 export default function useTestCategoriesSync(problems: Problem[]) {
   // Get all test categories in a list
   const testCategories = problems
@@ -11,7 +15,7 @@ export default function useTestCategoriesSync(problems: Problem[]) {
     .filter((c, index, arr) => arr.indexOf(c) === index);
 
   // On startup it syncs the test questions to the db
-  // DRAWBACK: Every user instance on a browser is a db query
+  // DRAWBACK: Page needs to be refreshed once
   useEffect(() => {
     (async () => {
       await syncTestCategories(testCategories, false);
@@ -22,14 +26,24 @@ export default function useTestCategoriesSync(problems: Problem[]) {
 // Sync test categories on startup
 async function syncTestCategories(category: string | string[], isActive: boolean) {
   const categories = Array.isArray(category) ? category : [category];
-  
+
   // Update or insert the existing categories to the db
   const { error } = await supabase.from("testcategories").upsert(
     categories.map((cat) => ({ category: cat, is_active: isActive })),
     { onConflict: "category", ignoreDuplicates: true },
   );
 
-  if (error) {
+   if (error) {
     throw new Error(error.message);
+  }
+
+  // Delete old test categories that are not in the problem submodules
+  const {error : deleteError } = await supabase
+    .from("testcategories")
+    .delete()
+    .not("category", "in", `(${categories.map((category) => `"${category}"`).join(",")})`);
+
+  if (deleteError) {
+    throw new Error(deleteError.message);
   }
 }
