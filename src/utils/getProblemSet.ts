8eac6ae57f552f143312890c162d92143bad827e
AltrosMado <vinay.sharma102@yahoo.com>
2026-05-19 12:43:50 -0400

    add way to import both sub modules at the same time

--- src/utils/getProblemSet.ts
+++ src/utils/getProblemSet.ts
@@ -1,19 +1,34 @@
+import { Problem } from "../types.js";
+
 /**
  * Here you define the different types of questions
  */
 const questionTypeList = ["private-problems", "public-problems"];
 
 /**
- * Depending on the REACT_APP_PROBLEM_SET, it returns am ESM module based off that ones
- * @returns ESM Module Import for the different problems
+ * Depending on the REACT_APP_PROBLEM_SET, it returns an ESM module import for the
+ * selected problems. If `REACT_APP_PROBLEM_SET` is set to "both", it imports
+ * both `private-problems` and `public-problems` concurrently and returns a
+ * merged array when both exports are arrays, otherwise returns an object with
+ * both sets keyed by their folder names.
  */
-async function getProblemSet() {
+async function getProblemSet(): Promise<Problem[]> {
   const questionType = process.env.REACT_APP_PROBLEM_SET;
+
+  //If env is set to both then it renders the both submodule problems
+  if (questionType === "both") {
+    const [privateSet, publicSet] = await Promise.all([
+      await import("../private-problems/problems.js").then((m) => m.default),
+      await import("../public-problems/problems.js").then((m) => m.default),
+    ]);
+      
+    return [...privateSet, ...publicSet];
+  }
 
   if (questionTypeList.includes(questionType!)) {
     return (await import(`../${questionType}/problems.js`)).default;
   } else {
-    throw Error("The env REACT_APP_PROBLEM_SET is incorrect or not set");
+    throw new Error("The env REACT_APP_PROBLEM_SET is incorrect or not set");
   }
 }
 
