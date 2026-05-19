67ba19ff22b311dd9044d3543a05d8a4014cebd7
Luke Weaver <bluefly102@gmail.com>
2026-05-19 11:21:23 -0400

    Revert "Revert to "Add TODOs""
    This reverts commit 939bffce8cfd2fbef9a562b79daefebd70d44f67.

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -96,6 +96,7 @@ export default function CategoryList({
     return (
         <List component="nav" sx={{ py: 2 }}>
             <>
+                {/* TODO: Add keyboard navigation to Blog Posts */}
                 <BlogMenuButton
                     category='blogs'
                     activeCategory={activeCategory}
