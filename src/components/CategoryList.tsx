1468e45139e430f9246bf7b9e9b4757d0eb17c28
Luke Weaver <bluefly102@gmail.com>
2026-05-21 10:09:03 -0400

    Switch the position of haystack and mutation in the category list

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -36,11 +36,11 @@ export default function CategoryList({
     .sort((a, b) => a.localeCompare(b));
 
   const specialCategories = [];
-  if (searchedProblems.some((c) => c.meta.question_type[0] === "mutation")) {
-    specialCategories.push("mutation");
-  }
   if (searchedProblems.some((c) => c.meta.question_type[0] === "haystack")) {
     specialCategories.push("haystack");
+  }
+  if (searchedProblems.some((c) => c.meta.question_type[0] === "mutation")) {
+    specialCategories.push("mutation");
   }
 
   const testCategories = searchedProblems
