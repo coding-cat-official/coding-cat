c345b9f0a6f66ad0bd7d1c73d5e730422a6df0ab
AltrosMado <vinay.sharma102@yahoo.com>
2026-05-25 13:13:58 -0400

    add err handling if profile id is not defined

--- src/components/profile/contract/Contract.tsx
+++ src/components/profile/contract/Contract.tsx
@@ -24,6 +24,10 @@ export default function Contract({ categoriesData, profileData }: ContractProps)
   const session = outletContext?.session;
   const profileId = profileData?.profile_id ?? session?.user.id;
 
+  if (!profileData) {
+     throw Error("Error: No session or profile_id has been defined");
+  }
+
   // if the categoriesData changes, update the problem count
   useEffect(() => {
     const probCountByCat = Object.fromEntries(
@@ -106,7 +110,7 @@ export default function Contract({ categoriesData, profileData }: ContractProps)
               })}`
             : "—"}
         </Typography>
-        <Button onClick={() => setOpen(true)}>View</Button>
+        <Button onClick={() => setOpen(true)}>View Contract</Button>
       </Stack>
 
       <ContractModal
