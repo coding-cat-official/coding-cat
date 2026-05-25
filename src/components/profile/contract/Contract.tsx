4f04a00cb9ab330b9fd591c01f0d0f604d0ad294
AltrosMado <vinay.sharma102@yahoo.com>
2026-05-25 15:50:35 -0400

    fix profile page not working

--- src/components/profile/contract/Contract.tsx
+++ src/components/profile/contract/Contract.tsx
@@ -24,7 +24,7 @@ export default function Contract({ categoriesData, profileData }: ContractProps)
   const session = outletContext?.session;
   const profileId = profileData?.profile_id ?? session?.user.id;
 
-  if (!profileData) {
+  if (!profileId) {
      throw Error("Error: No session or profile_id has been defined");
   }
 
