e5c15e34b8d48182b7353ac5689a33f4698f2b34
Luke Weaver <bluefly102@gmail.com>
2026-05-26 11:52:52 -0400

    Comment out unused var and add TODO

--- src/routes/ProblemView.tsx
+++ src/routes/ProblemView.tsx
@@ -15,7 +15,8 @@ import ReflectionInput from '../components/ReflectionInput';
 import CodingQuestion from '../components/CodingQuestion';
 import MutationQuestion from '../components/MutationQuestion';
 import { reflectionQuestions } from '../utils/questions';
-import { level0Questions } from '../utils/level0Questions';
+// TODO: See line 202
+// import { level0Questions } from '../utils/level0Questions';
 import Tutorial from '../components/MutationTutorial';
 import cursedCat from '../assets/cUrSed.png';
 import errorCat from '../assets/error-cat.png';
