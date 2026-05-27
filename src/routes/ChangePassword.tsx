fea49fecb8ccf4181dfdfd0bead123829e5dfd51
Luke Weaver <125918318+guitarwhal@users.noreply.github.com>
2026-05-27 10:25:49 -0400

    Auth fix (#267)
    * Switch to PKCE flow, which fixes redirect issues
    
    * Add fetchAdminStatus to deps array
    
    ---------
    
    Co-authored-by: Luke Weaver <bluefly102@gmail.com>

--- src/routes/ChangePassword.tsx
+++ src/routes/ChangePassword.tsx
@@ -1,41 +1,26 @@
 import { FormEvent, useState } from 'react'
 import { supabase } from '../supabaseClient'
 import { Box, Button, FormLabel, Input, Stack, Typography } from '@mui/joy';
-import { useLocation } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 
 /**
  * Change Password page for the app.
  */
 export default function ChangePassword() {
-  const location = useLocation();
-
   const [done, setDone] = useState(false);
-
   const [loading, setLoading] = useState(false);
   const [newPassword, setNewPassword] = useState("");
-
   const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
 
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
     setError("");
-    setSuccess("");
     setLoading(true);
 
-    const { access_token, refresh_token } = location.state || {};
-    if (access_token && refresh_token) {
-      await supabase.auth.setSession({ access_token, refresh_token });
-    }
-
-    const { error } = await supabase.auth.updateUser({
-      password: newPassword
-    });
+    const { error } = await supabase.auth.updateUser({ password: newPassword });
 
     if (error) {
       setError(error.message);
-      setLoading(false);
     } else {
       await supabase.auth.signOut();
       setDone(true);
@@ -43,15 +28,14 @@ export default function ChangePassword() {
     setLoading(false);
   }
 
-  // On success, navigate to signin
-  if(done) return <Navigate to='/signin' />
+  if (done) return <Navigate to='/signin' />;
 
   return (
     <Stack sx={{ flex: 3, width: "100%", marginBottom: "150px" }} direction="column" spacing="20px" justifyContent="center" alignItems="center">
       <Typography level="h2">Change Password</Typography>
       <form style={{ width: "25%", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", gap: "20px" }} onSubmit={handleSubmit}>
         <Box sx={{ width: "100%" }}>
-          <FormLabel>Password</FormLabel>
+          <FormLabel>New Password</FormLabel>
           <Input
             className="inputField"
             type="password"
@@ -65,8 +49,7 @@ export default function ChangePassword() {
           {loading ? <span>Loading</span> : <span>Change Password</span>}
         </Button>
       </form>
-      { !!error && <Typography color="danger">{error}</Typography> }
-      { !!success && <Typography color="success">{success}</Typography> }
+      {!!error && <Typography color="danger">{error}</Typography>}
     </Stack>
   )
 }
